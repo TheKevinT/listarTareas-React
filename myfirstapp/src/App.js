b4f619efb92986fb996dd35b55f091bae67be894
Kevin Tipantiza <kevin.tipantiza@epn.edu.ec>
2020-09-28 18:29:05 -0500

    crear componente formulario

--- myfirstapp/src/App.js
+++ myfirstapp/src/App.js
@@ -1,26 +1,72 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {todos} from './todos.json'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//como si trajeras informacion de un backend
+//console.log(todos);
+
+
+class App extends Component{
+
+  constructor(){
+    super();
+    this.state = {
+      todos
+    }
+   
+  }
+
+  render() {
+   const todos = this.state.todos.map((todo, i)=>{
+     return (
+     <div className="col-md-4">
+         <div className="card mt-4">
+       <div className="card-header">
+          <h3>{todo.title}</h3>
+     <span className="badge badge-spill badge-danger ml-2">{todo.priority}</span>
+       </div>
+
+       <div className="card-body">
+          <p>{todo.description}</p>
+          <p><mark>{todo.responsible}</mark></p>
+
+       </div>
+       </div>
+
+     </div>
+     )
+   })
+
+    return (
+   
+      <div className="App">
+          <nav className = "navbar navbar-dark bg-dark">
+              <a href="#" className ="text-white">Task
+              <span className="badge badge-pill badge-light ml-2">
+                { this.state.todos.length }
+              </span>
+              
+              </a>
+          </nav>
+
+          <div className="container">
+
+            <div className="row mt-4">
+
+                      { todos } 
+
+            </div>
+
+          </div>
+
+  
+          <img src={logo} className="App-logo" alt="logo" />
+        
+      </div>
+    );
+  }
+ 
 }
 
 export default App;
